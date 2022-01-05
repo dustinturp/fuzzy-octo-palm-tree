1e398a9ac782ea51b1f6c6119f8db430b1278bf3
dustinturp <dustinturp@outlook.com>
2022-01-04 20:05:14 -0600

    added files and hook

---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,24 @@
+import { createContext, useContext } from "react";
+import { useProductReducer } from "./reducers";
+
+const StoreContext = createContext();
+const { Provider } = StoreContext;
+
+// context obj comes with two components Provider and Consumer
+
+const StoreProvider = ({ value = [], ...props }) => {
+    const [state, dispatch] = useProductReducer({
+      products: [],
+      categories: [],
+      currentCategory: '',
+    });
+    // use this to confirm it works!
+    console.log(state);
+    return <Provider value={[state, dispatch]} {...props} />;
+  };
+
+const useStoreContext = () => {
+    return useContext(StoreContext);
+};
+
+export { StoreProvider, useStoreContext };
---
+++ client/src/utils/actions.js
@@ -0,0 +1,14 @@
+export const UPDATE_PRODUCTS = "UPDATE_PRODUCTS";
+// used by product list component
+
+export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
+//take list of categories received and store in global state
+
+export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
+// update from state created
+
+
+
+// defining how the state will be updated
+
+
